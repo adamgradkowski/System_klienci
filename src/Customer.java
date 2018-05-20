5045e4052853d86439d956cada5cb56b6c9a8387
adamgradkowski <adam.gradkowski1994@gmail.com>
2018-05-20 18:32:25 +0200

    Customer konstruktor

--- src/Customer.java
+++ src/Customer.java
@@ -5,4 +5,17 @@ public class Customer {
 	String lastName;
 	String pesel;
 	String MSISDN;
+	
+	public Customer(int id, String f, String l, String p, String M) {
+		idCustomer = id;
+		firstName = f;
+		lastName = l;
+		pesel = p;
+		MSISDN = M;
+	}
+	
+	void printInfo() {
+		String info = idCustomer + "; " + firstName  + "; " + lastName + "; " + pesel + "; " + MSISDN;
+		System.out.println(info);
+	}
 }
--- src/TabCustomers.java
+++ src/TabCustomers.java
@@ -4,18 +4,10 @@ public class TabCustomers {
 	public static void main(String[] args) {
 		final String appName= "Aplication vol1";
 		
-		Customer cust = new Customer();
-		cust.idCustomer = 1;
-		cust.firstName = "Adam";
-		cust.lastName = "Gradkowski";
-		cust.pesel = "94021615734";
-		cust.MSISDN = "798197372";
+		Customer cust = new Customer(1, "Adam", "Gradkowski", "94021615734", "798197372");
 		
 		System.out.println(appName);
-		System.out.println(cust.firstName);
-		System.out.println(cust.lastName);
-		System.out.println(cust.pesel);
-		System.out.println(cust.MSISDN);
+		cust.printInfo();
 
 	}
 
