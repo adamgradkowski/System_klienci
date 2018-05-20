186ee3db0e8b2fdc8c71bec1ebc1b82eed876b0d
adamgradkowski <adam.gradkowski1994@gmail.com>
2018-05-20 18:21:02 +0200

    TabCustomer

---
+++ src/TabCustomers.java
@@ -0,0 +1,21 @@
+
+public class TabCustomers {
+
+	public static void main(String[] args) {
+		final String appName= "Aplication vol1";
+		
+		int idCustomer = 1;
+		String firstName = "Adam";
+		String lastName = "Gradkowski";
+		String pesel = "94021615734";
+		String MSISDN = "798197372";
+		
+		System.out.println(appName);
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(pesel);
+		System.out.println(MSISDN);
+
+	}
+
+}
